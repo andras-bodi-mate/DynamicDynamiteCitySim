0502987b8cd31b6f484956fdb6b333267e5f5100
Szorenyi Zalan Andras <szorenyi.za@gmail.com>
2025-03-21 12:08:51 +0100

    Finished read_épületek_csv.py

--- src/read_épületek_csv.py
+++ src/read_épületek_csv.py
@@ -1,17 +1,47 @@
+from datetime import date
+from enum import Enum
 import os
-from building import Building
 import csv
-
-data=[]
-
+from dataclasses import dataclass, field
+@dataclass
+class ReadBuilding:
+    id: int
+    name: str
+    type: int
+    """
+    type:
+    1: lakóház
+    2: Oktatás(iskola)
+    3: Tűzoltóság 
+    4: Rendőrség
+    5: Kórház
+    6: Iroda
+    7: Park
+    8: Műv.ház
+    9: Egyéb(nem felismert)
+    """
+    constructionDate: date
+    usableArea: float
+    
 def read_dir():
+    postypes=["Lakóház","Iskola", "Tűzoltóság","Rendőrség","Kórház","Iroda","Park","Művelődésiház"]
+    buildings=[]
     source=input("Adja meg az épületek.csv fájl elérési útját!")    
     with open(source, mode='r', newline='') as file:
         reader = csv.reader(file)  
         for row in reader:        
             row[0]=int(float(row[0]))
             row[3]=int(float(row[3]))
-            print(row)
+            
+            try:
+                row[2]=(postypes.index(row[2])+1)
+            except:
+                row[2]=9
+            
+            
+            building=ReadBuilding(row[0],row[1],row[2],row[3],row[4])
+            buildings.append(building)
+            
             
             
 read_dir()
